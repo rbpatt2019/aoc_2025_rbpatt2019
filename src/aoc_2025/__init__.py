db18915730498b8068ee3ba2112a2bab0593eca4
rbpatt2019 <rb.patterson.cross@gmail.com>
2026-07-27 10:52:44 +0100

    feat(.): add day 1

--- src/aoc_2025/__init__.py
+++ src/aoc_2025/__init__.py
@@ -1,2 +1,22 @@
+"""AOC 2025 code."""
+
+from argparse import ArgumentParser
+from inspect import getmembers, isclass
+
+import aoc_2025.days as days
+
+
 def main() -> None:
-    print("Hello from aoc-2025!")
+    """Entrypoint for CLI."""
+    classes = {
+        name.lower(): member
+        for (name, member) in getmembers(
+            days, lambda c: isclass(c) and c.__module__ == days.__name__
+        )
+    }
+
+    parser = ArgumentParser(description="Advent of Code, 2025", suggest_on_error=True)
+    parser.add_argument("day", type=str, help="Which day to run", choices=["one"])
+    args = parser.parse_args()
+
+    print(classes[args.day]().run())
--- treefmt.nix
+++ treefmt.nix
@@ -4,6 +4,7 @@ _: {
     nixfmt.enable = true;
     deadnix.enable = true;
     statix.enable = true;
+    actionlint.enable = true;
     mdformat.enable = true;
     taplo.enable = true;
     ruff-format.enable = true;
