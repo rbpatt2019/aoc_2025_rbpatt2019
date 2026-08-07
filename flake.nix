ee8f6ecbeb54a9ee880992091116796df341d9cd
rbpatt2019 <rb.patterson.cross@gmail.com>
2026-08-07 17:11:14 +0100

    refactor(flake.nix): migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -1,8 +1,11 @@
+# adapted from https://github.com/lovesegfault/beautysh/blob/master/flake.nix
 {
   description = "hello world application using uv2nix";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     pyproject-nix = {
       url = "github:pyproject-nix/pyproject.nix";
@@ -32,142 +35,160 @@
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
+    inputs@{
+      flake-parts,
       pyproject-nix,
       uv2nix,
       pyproject-build-systems,
-      git-hooks,
-      treefmt-nix,
       ...
     }:
-    let
-      inherit (nixpkgs) lib;
-      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
-
-      workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
-
-      overlay = workspace.mkPyprojectOverlay {
-        sourcePreference = "wheel";
-      };
-
-      editableOverlay = workspace.mkEditablePyprojectOverlay {
-        root = "$REPO_ROOT";
-      };
-
-      pythonSets = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          python = pkgs.python3;
-        in
-        (pkgs.callPackage pyproject-nix.build.packages {
-          inherit python;
-        }).overrideScope
-          (
-            lib.composeManyExtensions [
-              pyproject-build-systems.overlays.wheel
-              overlay
-            ]
-          )
-      );
-
-    in
-    {
-      checks = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-        in
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks.flakeModule
+      ];
+      systems = [ "aarch64-darwin" ];
+      perSystem =
         {
-          pre-commit-check = git-hooks.lib.${system}.run {
-            src = ./.;
-            package = pkgs.prek;
-            # we do not use treefmt-nix here, as it isn't supported
-            # but we enable all the same checks
-            # use `nix fmt` to make changes
-            hooks = {
-              check-added-large-files.enable = true;
-              check-executables-have-shebangs.enable = true;
-              check-shebang-scripts-are-executable.enable = true;
-              check-merge-conflicts.enable = true;
-              detect-private-keys.enable = true;
-              end-of-file-fixer.enable = true;
-              mixed-line-endings.enable = true;
-              trim-trailing-whitespace.enable = true;
-              deadnix.enable = true;
-              statix.enable = true;
-              nixfmt.enable = true;
-              actionlint.enable = true;
-              taplo.enable = true;
-              mdformat.enable = true;
-              ruff-format.enable = true;
-              ruff.enable = true;
-              uv-check.enable = true;
-              uv-audit = {
-                enable = true;
-                name = "uv audit";
-                entry = "${pkgs.uv}/bin/uv audit";
-                types = [ "python" ];
-                pass_filenames = false;
-              };
-            };
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+          overlay = workspace.mkPyprojectOverlay {
+            sourcePreference = "wheel";
           };
-        }
-      );
+          editableOverlay = workspace.mkEditablePyprojectOverlay {
+            root = "$REPO_ROOT";
+          };
+          mkPythonSet =
+            python:
+            (pkgs.callPackage pyproject-nix.build.packages { inherit python; }).overrideScope (
+              lib.composeManyExtensions [
+                pyproject-build-systems.overlays.wheel
+                overlay
+              ]
+            );
 
-      formatter = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
-        in
-        treefmtEval.config.build.wrapper
-      );
+          pythonSet = mkPythonSet pkgs.python3;
+          devPythonSet = pythonSet.overrideScope editableOverlay;
 
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          pythonSet = pythonSets.${system}.overrideScope editableOverlay;
-          virtualenv = pythonSet.mkVirtualEnv "dev_aoc_2025_rbpatt2019" workspace.deps.all;
-          inherit (self.checks.${system}.pre-commit-check) shellHook enabledPackages;
+          venv = pythonSet.mkVirtualEnv "aoc_2025" workspace.deps.default;
+          buildVenv = pythonSet.mkVirtualEnv "build_aoc_2025" {
+            aoc-2025 = [ "build" ];
+          };
+          devVenv = devPythonSet.mkVirtualEnv "dev_aoc_2025" workspace.deps.all;
+
+          inherit (pkgs.callPackage pyproject-nix.build.util { }) mkApplication;
         in
         {
-          default = pkgs.mkShell {
-            buildInputs = enabledPackages;
+          packages.default = mkApplication {
+            inherit venv;
+            package = pythonSet.aoc-2025;
+          };
+
+          checks =
+            let
+              fs = lib.fileset;
+              pyproject = fs.unions [
+                ./pyproject.toml
+                ./uv.lock
+              ];
+              runCommand =
+                name: command:
+                pkgs.runCommand name {
+                  buildInputs = [ buildVenv ];
+                  src = fs.toSource {
+                    root = ./.;
+                    fileset = pyproject;
+                  };
+                  postInstall = ''
+                    cp -vr . $out
+                  '';
+                } command;
+            in
+            {
+              # drv is RO, so this will fail if there are updates
+              uv-lock = runCommand "uv-lock" ''
+                ${buildVenv}/bin/uv lock --project $src \
+                --upgrade --no-cache -p ${buildVenv}/bin/python &&
+                touch $out
+              '';
+              uv-audit = runCommand "uv-audit" ''
+                ${buildVenv}/bin/uv audit --project $src \
+                --preview-features audit-command --no-cache --frozen --no-dev &&
+                touch $out
+              '';
+            };
+
+          devShells.default = pkgs.mkShell {
             packages = [
-              virtualenv
-              pkgs.uv
-            ];
+              devVenv
+            ]
+            ++ (builtins.attrValues config.treefmt.build.programs)
+            ++ config.pre-commit.settings.enabledPackages;
+
             env = {
               UV_NO_SYNC = "1";
-              UV_PYTHON = pythonSet.python.interpreter;
+              UV_PYTHON = devPythonSet.python.interpreter;
               UV_PYTHON_DOWNLOADS = "never";
             };
+
             shellHook = ''
               unset PYTHONPATH
               export REPO_ROOT=$(git rev-parse --show-toplevel)
-            ''
-            + shellHook;
+              ${config.pre-commit.installationScript}
+            '';
           };
-        }
-      );
 
-      packages = forAllSystems (
-        system:
-        let
-          pythonSet = pythonSets.${system};
-          pkgs = nixpkgs.legacyPackages.${system};
-          inherit (pkgs.callPackages pyproject-nix.build.util { }) mkApplication;
-        in
-        {
-          default = mkApplication {
-            venv = pythonSet.mkVirtualEnv "aoc_2025_rbpatt2019" workspace.deps.default;
-            package = pythonSet.aoc-2025;
+          treefmt = {
+            projectRootFile = "flake.nix";
+            flakeFormatter = true;
+            flakeCheck = false; # handled by pre-commit
+            programs = {
+              nixfmt.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+              ruff-format = {
+                enable = true;
+                lineLength = 88;
+              };
+              mdformat.enable = true;
+              yamlfmt.enable = true;
+              taplo.enable = true;
+            };
           };
-        }
-      );
+
+          pre-commit.settings = {
+            package = pkgs.prek;
+            hooks = {
+              check-added-large-files.enable = true;
+              check-merge-conflicts.enable = true;
+              end-of-file-fixer.enable = true;
+              mixed-line-endings.enable = true;
+              trim-trailing-whitespace.enable = true;
+              ripsecrets.enable = true;
+              treefmt.enable = true;
+              flake-checker.enable = true;
+              ruff = {
+                enable = true;
+                entry = lib.mkForce "${devVenv}/bin/ruff check";
+              };
+              uv-check = {
+                enable = true;
+                entry = lib.mkForce "${devVenv}/bin/uv check";
+                pass_filenames = false;
+              };
+              nix-flake-check = {
+                enable = true;
+                entry = "nix flake check --no-warn-dirty .";
+                pass_filenames = false;
+                stages = [ "pre-push" ];
+              };
+            };
+          };
+        };
     };
 }
