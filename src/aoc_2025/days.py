1a4778ed3b68780ef5cdcca7b24fe82e360e030e
rbpatt2019 <rb.patterson.cross@gmail.com>
2026-09-17 15:10:01 +0100

    feat(days.py): add day 3a

--- src/aoc_2025/days.py
+++ src/aoc_2025/days.py
@@ -2,6 +2,7 @@
 
 import re
 from dataclasses import dataclass
+from itertools import combinations
 
 from .base_class import STATIC, Day
 
@@ -109,3 +110,28 @@ class Two(Day):
 
         pattern = re.compile(r"(.+)\1+")
         return str(sum(x for span in ranges for x in span if pattern.fullmatch(str(x))))
+
+
+@dataclass
+class Three(Day):
+    """Day three."""
+
+    def a(self) -> str:
+        """Find the largest pair of numbers in a string.
+
+        Returns:
+            str: The sum of all maximum pairs.
+        """
+        with open(STATIC / "3.txt") as file:
+            batteries = [line.strip() for line in file]
+
+        return str(
+            sum(
+                int("".join(sorted(combinations(bank, 2), reverse=True)[0]))
+                for bank in batteries
+            )
+        )
+
+    def b(self) -> str:
+        """Hold."""
+        return "hold"
