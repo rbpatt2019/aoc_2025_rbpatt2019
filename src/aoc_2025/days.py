c278d2277985e1b7569183d43ec3907fa568c02a
Ryan Patterson-Cross <43184541+rbpatt2019@users.noreply.github.com>
2026-09-18 10:19:16 +0100

    feat(days.py): add day 4 code
    * feat(days.py): add day 4a code
    
    * refactor(days.py): return coordiantes
    
    To allow iterative operations, we yield the coordinate, then retrieve
    count using len
    
    * feat(days.py): add day 4b code
    
    * build(uv.lock): update

--- src/aoc_2025/days.py
+++ src/aoc_2025/days.py
@@ -3,6 +3,7 @@
 import re
 from dataclasses import dataclass
 from itertools import combinations
+from typing import Generator
 
 from .base_class import STATIC, Day
 
@@ -156,3 +157,70 @@ class Three(Day):
             return int(val)
 
         return str(sum(_max(bank, 12) for bank in batteries))
+
+
+def _count_surround(
+    data: list[list[str]], val: str, threshold: int
+) -> Generator[tuple[int, int], None, None]:
+    """Determine if there are too many elements around a value.
+
+    For a 2D grid, at each point, check how many of the surrounding points are ``val``.
+    Then, check if that is >= threshold.
+    If less than threshould, return the coordinates.
+
+    Args:
+        data (list[list[str]]): a 2D data grid
+        val (str): the check value
+        threshold (int): how many cells is too many
+
+    Returns:
+        Generator[tuple[int, int], None, None]: generator of coordinates.
+    """
+    search = [(i, j) for i in (-1, 0, 1) for j in (-1, 0, 1) if not (i == j == 0)]
+
+    for x, row in enumerate(data):
+        for y, cell in enumerate(row):
+            if cell != val:
+                continue
+            count = [
+                data[x + i][y + j] == val
+                for i, j in search
+                # boundary check
+                if (0 <= x + i < len(data) and 0 <= y + j < len(row))
+            ]
+            if sum(count) < threshold:
+                yield x, y
+
+
+@dataclass
+class Four(Day):
+    """Day four."""
+
+    def a(self) -> str:
+        """Find the number of @ signs around a point.
+
+        Returns:
+            str: The number of rolls with 4 or less surrounding.
+        """
+        with open(STATIC / "4.txt") as file:
+            grid = [list(line.strip()) for line in file.readlines()]
+        return str(len(list(_count_surround(grid, "@", 4))))
+
+    def b(self) -> str:
+        """Find total number of rolls.
+
+        It's unclear to me from the prompt whether this is a greedy search or an optimised solution.
+        Choosing to be greedy. Just keep running until length is 0.
+
+        Returns:
+            str: The number of rolls that can be removed.
+        """
+        with open(STATIC / "4.txt") as file:
+            grid = [list(line.strip()) for line in file.readlines()]
+
+        count = 0
+        while len(rolls := list(_count_surround(grid, "@", 4))) > 0:
+            count += len(rolls)
+            for x, y in rolls:
+                grid[x][y] = "."
+        return str(count)
